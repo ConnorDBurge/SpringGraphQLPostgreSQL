e13c44c8e0c8a0e6c484acb8c04601fa78cb7941
ConnorDBurge <connor.d.burge@gmail.com>
2022-04-16 22:13:12 -0700

    started Spring Boot/GraphQL/PostgreSQL project

---
+++ src/main/java/com/egrubc/springgraphqlpostgresql/SpringGraphQlPostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package com.egrubc.springgraphqlpostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphQlPostgreSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGraphQlPostgreSqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/egrubc/springgraphqlpostgresql/SpringGraphQlPostgreSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egrubc.springgraphqlpostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphQlPostgreSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
